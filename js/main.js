8133ebf945ed4566ee580b25ea20a8b0ff3788e4
Maarten Billemont <lhunath@lyndir.com>
2021-12-02 00:34:11 -0500

    Initial prototype of the new Spectre web application.
    [ADDED]     Spectre base web application user interface based on the Spectre homepage design.
    [ADDED]     Spectre JavaScript implementation, including types, algorith, a web worker and a service.
    [REMOVED]   Original Master Password web application and implementation.

--- js/main.js
+++ js/main.js
@@ -1,115 +1,81 @@
-var mpw, error;
+function spinnerBusy(element, busy) {
+    let spin = Math.max(0, (element.data("spin") || 0) + (busy ? 1 : -1));
+    element.data("spin", spin);
 
-function updateMPW() {
-    mpw = null;
-    startWork();
-    mpw = new MPW( $('#userName')[0].value, $('#masterPassword')[0].value, $('#version')[0].value );
-    mpw.key.then(
-        function() {
-            doneWork();
-        },
-        function(reason) {
-            error = reason;
-            mpw = null;
-            doneWork();
+    if (spin) {
+        element.finish().fadeIn();
+    } else {
+        element.finish().fadeOut();
+    }
+}
+
+$(function () {
+    let signInButton = $('#signin');
+    let signOutButton = $('#signout');
+    let user = $('#user');
+    let userName = user.find('#userName');
+    let userNameInput = userName.find('input');
+    let userSecret = user.find('#userSecret');
+    let userSecretInput = userSecret.find('input');
+    let userSecretSpinner = userSecret.find('.fa-spin');
+    let userAlgorithm = user.find('#algorithmVersion');
+    let userAlgorithmInput = userAlgorithm.find('select');
+    let userMessage = user.find('p.info');
+    let userError = user.find('p.error');
+    let site = $('#site');
+    let siteName = site.find('#siteName');
+    let siteNameInput = siteName.find('input');
+    let sitePassword = site.find('#sitePassword');
+    let sitePasswordSpinner = sitePassword.find('.fa-spin');
+    let sitePasswordButton = sitePassword.find('button');
+    let sitePasswordInput = sitePasswordButton.find('input')
+    let siteMessage = site.find('p.info');
+    let siteError = site.find('p.error');
+
+    signInButton.on('click', function () {
+        spinnerBusy(userSecretSpinner, true);
+
+        spectre.authenticate(userNameInput[0].value, userSecretInput[0].value, userAlgorithmInput[0].value)
+    });
+
+    signOutButton.on('click', function () {
+        spinnerBusy(userSecretSpinner, true);
+
+        spectre.invalidate()
+    });
+
+    siteName.on('input', function () {
+        spinnerBusy(sitePasswordSpinner, true);
+
+        spectre.password(siteNameInput[0].value)
+    });
+    
+    function updateView() {
+        if (!spectre.authenticated) {
+            userAlgorithmInput.val(spectre.algorithm.current);
+            siteNameInput.val(null);
         }
-    );
-}
-function startWork() {
-    update(true);
-}
-function doneWork() {
-    update(false);
-}
-function update(working) {
-    var screen = mpw? 'site': 'identity';
 
-    // Screen Name
-    if (screen == 'identity') {
-        $('#identity').addClass('active');
+        user.attr("data-active", !spectre.authenticated);
+        site.attr("data-active", spectre.authenticated);
 
-        if (!working)
-            $('#userName').focus();
+        userNameInput.val(spectre.userName);
+        userSecretInput.val(null);
+        sitePasswordInput.val(spectre.result(siteNameInput[0].value));
     }
-    else {
-        $('#identity').removeClass('active');
-        $('#userName')[0].value = $('#masterPassword')[0].value = '';
-    }
+    spectre.observers.push(updateView);
+    updateView()
 
-    if (screen == 'site') {
-        $('#site').addClass('active');
+    sitePasswordButton.on('click', function () {
+        sitePasswordInput.select()
+        document.execCommand('copy');
 
-        if (!working)
-            $('#siteName').focus();
-    }
-    else {
-        $('#site').removeClass('active');
-        $('#siteName').val(null);
-        $('#sitePassword').text(null);
-    }
-
-    // Working
-    if (working && screen == 'identity')
-        $('#identity').addClass('working').find('input, select').attr('disabled', 'disabled');
-    else
-        $('#identity').removeClass('working').find('input, select').removeAttr('disabled');
-
-    if (working && screen == 'site')
-        $('#site').addClass('working');
-    else
-        $('#site').removeClass('working');
-
-    // Error
-    $('#error').text(error);
-}
-function updateSite() {
-    if (!mpw) {
-        doneWork();
-        return
-    }
-
-    startWork();
-    mpw.generatePassword( $('#siteName')[0].value, $('#siteCounter')[0].valueAsNumber, $('#siteType')[0].value )
-       .then( function (sitePassword) {
-           $('#sitePassword').text(sitePassword);
-           doneWork();
-       }, function (reason) {
-           error = reason;
-           doneWork();
-       });
-}
-function selectText(element) {
-    var doc = document, range, selection;    
-
-    if (doc.body.createTextRange) { //ms
-        range = doc.body.createTextRange();
-        range.moveToElementText(element);
-        range.select();
-    } else if (window.getSelection) { //all others
-        selection = window.getSelection();        
-        range = doc.createRange();
-        range.selectNodeContents(element);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-}
-
-
-$(function() {
-    $('#identity form').on('submit', function() {
-        updateMPW();
-        return false;
-    });
-    $('#site input, #site select').on('change input keyup', function() {
-        updateSite();
-    });
-    $('#logout').on('click', function() {
-        mpw = null;
-        doneWork();
-    });
-    $('#sitePassword').on('click', function() {
-        selectText(this);
+        sitePasswordButton.attr("title", "Copied!").tooltip("_fixTitle").tooltip("show");
+        setTimeout(function () {
+            sitePasswordButton.tooltip("hide").attr("title", "Copy Password").tooltip("_fixTitle");
+        }, 1000);
     });
 
-    doneWork();
+    spinnerBusy(userSecretSpinner, false);
+    spinnerBusy(sitePasswordSpinner, false);
 });
